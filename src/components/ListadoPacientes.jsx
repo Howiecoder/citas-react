c54b899ffc3e93a8508927e194a2b425777fce47
Howiecoder <coderhowie@gmail.com>
2023-08-24 06:59:05 -0300

    talwindconfig

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -4,11 +4,11 @@ import Paciente from "../Paciente"
  const ListadoPacientes = () => {
     return (
    
-      <div className=" md:w-1/2 lg:w-3/5 md:h-screen overflow-scroll">
+      <div className=" md:w-1/2 lg:w-2/5 md:h-screen overflow-scroll">
 
         <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
 
-        <p className="text-xl mt-5 mb-10 text-center">Administra tus {''}
+        <p className="text-xl mt-5 mb-3 text-center">Administra tus {''}
         <span className="text-indigo-600 font-bold">Pacientes y Citas
         </span>
         </p>
